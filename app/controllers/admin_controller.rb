f0fe28d1fe82082715c9bbffc7ae4a3b164c253e
katoy <youichikato@gmail.com>
2013-07-10 00:23:27 +0900

    Add password fild in /admin/add_new_use

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,14 +10,22 @@ class AdminController < ApplicationController
   end
   
   def add_new_user
+    pwd = nil
+    use_random_pwd = false
     if params[:user]
-      pwd = SecureRandom.hex(8) # user will need to use the "forgot password" feature to set a useful password
+      pwd = params[:user][:password]
+      if pwd == nil or pwd.length < 8
+        pwd = SecureRandom.hex(8) # user will need to use the "forgot password" feature to set a useful password
+        use_random_pwd = true
+      end
       options = {:username => params[:user][:username], :email => params[:user][:email], :password => pwd, :password_confirmation => pwd, :role => User.user_role} if params[:user]
       user = User.new(options)
       unless user.save
         @error_message = user.errors.full_messages.map{|s| s}.join('<br />') if user.errors
         @error_message ||= t(:cannot_save_new_user, :scope => 'myinfo.errors.messages')
       end
+      @error_message = "#{@error_message}<br />Use random string for password.(Because inputed password length < 8)" if use_random_pwd
+
     else
       @error_message ||= t(:cannot_save_new_user, :scope => 'myinfo.errors.messages')
     end
